0de648227fd4126cdbc537769d82227a6c2a1461
NB419 <77996450+NB419@users.noreply.github.com>
2021-03-04 13:17:08 -0500

    Update Watermark.java

--- src/main/java/com/gamesense/client/module/modules/hud/Watermark.java
+++ src/main/java/com/gamesense/client/module/modules/hud/Watermark.java
@@ -39,7 +39,7 @@ public class Watermark extends HUDModule {
 
 		@Override
 		public String getItem(int index) {
-			return "PikkelHack "+ GameSense.MODVER;
+			return "PikkelHack " + GameSense.MODVER;
 		}
 
 		@Override
